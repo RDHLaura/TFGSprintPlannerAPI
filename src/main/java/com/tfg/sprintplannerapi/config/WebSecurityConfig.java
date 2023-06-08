e42319232c9a95adcd73e2769560e05d1356f750
rdhlaura <laurarodriguezdeharo@outlook.es>
2023-06-08 23:12:37 +0200

    Revert "cors deploy"
    This reverts commit fe8f2397eafee49ee1176b3e1213763adde91c81.

--- src/main/java/com/tfg/sprintplannerapi/config/WebSecurityConfig.java
+++ src/main/java/com/tfg/sprintplannerapi/config/WebSecurityConfig.java
@@ -19,9 +19,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -62,7 +65,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("https://sprintplanner-tfg.netlify.app/"));
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+        //configuration.setAllowedOrigins(Arrays.asList("/**"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
         configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type"));
          configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type"));
