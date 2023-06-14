df92615445405a910f91ef3c238b34c2a87daa5e
rdhlaura <laurarodriguezdeharo@outlook.es>
2023-06-14 14:50:51 +0200

    flayway

--- src/main/java/com/tfg/sprintplannerapi/bo/ImageBO.java
+++ src/main/java/com/tfg/sprintplannerapi/bo/ImageBO.java
@@ -21,7 +21,7 @@ public class ImageBO {
      */
     public String uploadImage( MultipartFile image){
         if(!image.isEmpty()){
-            Path directoryImage = Paths.get("src//main//resources//static//uploads");
+            Path directoryImage = Paths.get("//src//main//resources//static//uploads");
             String absoluteRoute = directoryImage.toFile().getAbsolutePath();
             try{
                 byte[] byteImg = image.getBytes();
@@ -44,7 +44,7 @@ public class ImageBO {
      */
     public Resource loadImage(String avatarName) {
         if(avatarName != null) {
-            Path imagePath = Paths.get("src//main//resources//static//uploads//" + avatarName);
+            Path imagePath = Paths.get("//src//main//resources//static//uploads//" + avatarName);
             Resource imageResource;
             imageResource = new FileSystemResource(imagePath);
 
